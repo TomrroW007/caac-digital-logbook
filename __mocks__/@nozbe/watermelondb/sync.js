d0d6e3e90c68093244868b884e25ae5e394ceaee
Robin.H <110992957+TomrroW007@users.noreply.github.com>
2026-03-10 11:32:49 +0800

    Revert "Feature/phase7 data migration cloud sync (#27)"
    This reverts commit 52eeb55ad3fca61aefd2299796f9a1a7cfec921d.

--- __mocks__/@nozbe/watermelondb/sync.js
+++
@@ -1,8 +0,0 @@
-/**
- * Mock for @nozbe/watermelondb/sync
- * Provides a no-op synchronize function for Jest test environment.
- */
-
-const synchronize = jest.fn().mockResolvedValue(undefined);
-
-module.exports = { synchronize };
